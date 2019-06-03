5594cf53def5e5f0479d8296051f4111577fe16d
culbert-alexander <alexander.culbert@duke.edu>
2019-06-03 16:46:28 -0400

    Initial Commit

---
+++ app/src/androidTest/java/com/example/abvforyourbuck/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.example.abvforyourbuck
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.example.abvforyourbuck", appContext.packageName)
+    }
+}
