05d830c27c0cb871bd5b07d34ee9aa6d4319f060
culbert-alexander <alexander.culbert@duke.edu>
2019-06-04 19:03:58 -0400

    Added more buttons

--- app/src/main/java/com/example/abvforyourbuck/MainActivity.kt
+++ app/src/main/java/com/example/abvforyourbuck/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.abvforyourbuck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun openABC(view: View) {
+
+    }
+    fun openCC(view: View) {
+
+    }
+    fun openHT(view: View) {
+
+    }
+    fun openSZ(view: View) {
+
+    }
+    fun openTG(view: View) {
+
+    }
+    fun openWM(view: View) {
+
     }
 }
